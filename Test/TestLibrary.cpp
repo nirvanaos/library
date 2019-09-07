d3ddbc6b9c158826690c237db95d6b7cad798213
Igor Popov <popov@intromarin.ru>
2019-09-07 14:27:13 +0300

    Runnable test has been moved to ORB.

--- Test/TestLibrary.cpp
+++ Test/TestLibrary.cpp
@@ -1,8 +1,5 @@
 #include <gtest/gtest.h>
 #include <Nirvana/bitutils.h>
-#include <CORBA/Servant.h>
-#include <Nirvana/Runnable_s.h>
-#include <functional>
 
 namespace TestLibrary {
 
@@ -109,36 +106,6 @@ TEST_F (TestLibrary, Log2)
 	EXPECT_EQ (l2_3, 2);
 	EXPECT_EQ (l2_4, 2);
 	EXPECT_EQ (l2_5, 3);
-}
-
-class Functor :
-	public CORBA::Nirvana::Servant <Functor, Runnable>,
-	public CORBA::Nirvana::LifeCycleStatic <>
-{
-public:
-	Functor (const std::function <void ()>& f) :
-		func_ (f)
-	{}
-
-	void run ()
-	{
-		func_ ();
-	}
-
-private:
-	std::function <void ()> func_;
-};
-
-TEST_F (TestLibrary, Runnable)
-{
-	using namespace std;
-
-	int a = 1, b = 2, c = 0;
-
-	Functor functor ([a, b, &c]() { c = a + b; });
-	Runnable_ptr r = &functor;
-	r->run ();
-	EXPECT_EQ (c, a + b);
 }
 
 }
