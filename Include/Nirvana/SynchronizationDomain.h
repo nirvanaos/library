19c316963474c9ebd63c6888ee3f7bdb1eada992
Igor Popov <silver.popov@gmail.com>
2020-02-18 19:09:15 +0300

    SynchronizationDomain.h

--- Include/Nirvana/SynchronizationDomain.h
+++ Include/Nirvana/SynchronizationDomain.h
@@ -15,12 +15,22 @@ class SynchronizationDomain;
 
 /*
 pseudo interface SynchronizationDomain {
-  enter (ContextFrame frame);
+  /// Copy inout data from the current domain memory to target domain memory
+  Pointer copy_inout (ConstPointer src, UWord cb);
+
+	/// Move out (ret) data from target domain memory to current domain memory
+	/// src memory will be released
+	Pointer move_out (Pointer src, UWord cb);
+
+	/// Enter to the target synchronization domain
+	enter (ContextFrame frame);
+
+	/// Return to the current synchronization domain
 	leave (ContextFrame frame);
 
-	// Call runnable in the new execution domain.
-	// Deadline by default is equal to the caller deadline.
-	// To change the deadline use Current::set_next_async_deadline().
+	/// Call runnable in the new execution domain.
+	/// Deadline by default is equal to the caller deadline.
+	/// To change the deadline use Current::set_next_async_deadline().
 	async_call (Runnable runnable);
 };
 */
@@ -35,6 +45,8 @@ namespace CORBA {
 namespace Nirvana {
 
 BRIDGE_BEGIN (::Nirvana::SynchronizationDomain)
+::Nirvana::Pointer (*copy_inout) (Bridge <::Nirvana::SynchronizationDomain>*, ::Nirvana::ConstPointer src, ::Nirvana::UWord cb, EnvironmentBridge*);
+::Nirvana::Pointer (*move_out) (Bridge <::Nirvana::SynchronizationDomain>*, ::Nirvana::Pointer src, ::Nirvana::UWord cb, EnvironmentBridge*);
 void (*enter) (Bridge <::Nirvana::SynchronizationDomain>*, ::Nirvana::ContextFrame*, EnvironmentBridge*);
 void (*leave) (Bridge <::Nirvana::SynchronizationDomain>*, ::Nirvana::ContextFrame*, EnvironmentBridge*);
 void (*async_call) (Bridge <::Nirvana::SynchronizationDomain>*, Bridge <::Nirvana::Runnable>*, EnvironmentBridge*);
@@ -45,8 +57,22 @@ class Client <T, ::Nirvana::SynchronizationDomain> :
 	public T
 {
 public:
+	/// Copy inout data from the current domain memory to target domain memory
+	::Nirvana::Pointer copy_inout (::Nirvana::ConstPointer src, ::Nirvana::UWord cb);
+
+	/// Move out (ret) data from target domain memory to current domain memory
+	/// src memory will be released
+	::Nirvana::Pointer move_out (::Nirvana::Pointer src, ::Nirvana::UWord cb);
+
+	/// Enter to the target synchronization domain
 	void enter (::Nirvana::ContextFrame& context);
+
+	/// Return to the current synchronization domain
 	void leave (::Nirvana::ContextFrame& context);
+
+	/// Call runnable in the new execution domain.
+	/// Deadline by default is equal to the caller deadline.
+	/// To change the deadline use Current::set_next_async_deadline().
 	void async_call (::Nirvana::Runnable_ptr runnable);
 };
 
