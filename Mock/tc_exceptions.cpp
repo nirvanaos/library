63e2c9c23d5ab83c7b68f4a80eda082b1d4def6d
Igor Popov <silver.popov@gmail.com>
2020-06-04 16:19:27 +0300

    Add TC constants to the Mock library.

---
+++ Mock/tc_exceptions.cpp
@@ -0,0 +1,16 @@
+#include <CORBA/CORBA.h>
+#include <Nirvana/OLF.h>
+
+namespace CORBA {
+
+#define IMPORT_TC(E) extern const ::Nirvana::ImportInterfaceT <TypeCode> _tc_##E = { ::Nirvana::OLF_IMPORT_INTERFACE };
+
+SYSTEM_EXCEPTIONS (IMPORT_TC)
+
+IMPORT_TC (UnknownUserException)
+
+const ::Nirvana::ImportInterfaceT <TypeCode> TypeCode::_tc_BadKind = { ::Nirvana::OLF_IMPORT_INTERFACE };
+
+const ::Nirvana::ImportInterfaceT <TypeCode> TypeCode::_tc_Bounds = { ::Nirvana::OLF_IMPORT_INTERFACE };
+
+}
