b7397d98366de6bfbd57e8dcdec982f5a7e79b10
Igor Popov <silver.popov@gmail.com>
2020-11-21 17:46:39 +0300

    malloc.cpp

--- CRTL/malloc.cpp
+++ CRTL/malloc.cpp
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+//#include <stdlib.h>
 #include <Nirvana/Nirvana.h>
 
 namespace Nirvana {
@@ -28,6 +28,16 @@ void* malloc (size_t size)
 	return nullptr;
 }
 
+void* _malloc_base (size_t size)
+{
+	return malloc (size);
+}
+
+void* _malloc_dbg (size_t size, int blockType, const char* filename, int linenumber)
+{
+	return malloc (size);
+}
+
 void* calloc (size_t num, size_t size)
 {
 	void* mem = g_memory->allocate (nullptr, num * size + sizeof (HeapBlockHdr), Memory::EXACTLY | Memory::ZERO_INIT);
@@ -38,12 +48,35 @@ void* calloc (size_t num, size_t size)
 	return nullptr;
 }
 
+void* _calloc_base (size_t num, size_t size)
+{
+	return calloc (num, size);
+}
+
+void* _calloc_dbg (size_t num, size_t size, int blockType, const char* filename, int linenumber)
+{
+	return calloc (num, size);
+}
+
+void _free_base (void* p)
+{
+	if (p) {
+		HeapBlockHdr* block = (HeapBlockHdr*)p - 1;
+		g_memory->release (block, block->size);
+	}
+}
+
 void free (void* p)
 {
 	if (p) {
 		HeapBlockHdr* block = (HeapBlockHdr*)p - 1;
 		g_memory->release (block, block->size);
 	}
+}
+
+void _free_dbg (void* p, int _BlockUse)
+{
+	free (p);
 }
 
 }
