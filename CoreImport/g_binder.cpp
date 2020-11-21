701613455335ea85a54d8f2046f5cb79c1221b46
Igor Popov <silver.popov@gmail.com>
2020-11-21 06:23:06 +0300

    Restructuring.

---
+++ CoreImport/g_binder.cpp
@@ -0,0 +1,9 @@
+#include <Nirvana/core_objects.h>
+#include <Nirvana/OLF.h>
+
+namespace Nirvana {
+
+NIRVANA_OLF_SECTION
+extern const ImportInterfaceT <Binder> g_binder = { OLF_IMPORT_INTERFACE, "Nirvana/g_binder", Binder::repository_id_ };
+
+}
