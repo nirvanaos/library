2904907625da42c65bc78a3f42984b53918ccfc1
silver-popov <silver.popov@gmail.com>
2021-03-26 17:01:16 +0300

    + struct Definitions

--- Mock/tc_exceptions.cpp
+++ Mock/tc_exceptions.cpp
@@ -9,8 +9,8 @@ SYSTEM_EXCEPTIONS (IMPORT_TC)
 
 IMPORT_TC (UnknownUserException)
 
-const ::Nirvana::ImportInterfaceT <TypeCode> TypeCode::_tc_BadKind = { ::Nirvana::OLF_IMPORT_INTERFACE };
+const ::Nirvana::ImportInterfaceT <TypeCode> CORBA::Nirvana::Definitions <TypeCode>::_tc_BadKind = { ::Nirvana::OLF_IMPORT_INTERFACE };
 
-const ::Nirvana::ImportInterfaceT <TypeCode> TypeCode::_tc_Bounds = { ::Nirvana::OLF_IMPORT_INTERFACE };
+const ::Nirvana::ImportInterfaceT <TypeCode> CORBA::Nirvana::Definitions <TypeCode>::_tc_Bounds = { ::Nirvana::OLF_IMPORT_INTERFACE };
 
 }
