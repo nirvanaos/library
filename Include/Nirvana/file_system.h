48a47bca7b03c2377b188f9e4787f287e69a6f86
Igor Popov <popov.nirvana@gmail.com>
2022-01-11 07:40:02 +0300

    + FileFactory

---
+++ Include/Nirvana/file_system.h
@@ -0,0 +1,37 @@
+/// \file
+/// \brief Nirvana native types
+/*
+* Nirvana runtime library.
+*
+* This is a part of the Nirvana project.
+*
+* Author: Igor Popov
+*
+* Copyright (c) 2021 Igor Popov.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Send comments and/or bug reports to:
+*  popov.nirvana@gmail.com
+*/
+#ifndef NIRVANA_FILE_SYSTEM_H_
+#define NIRVANA_FILE_SYSTEM_H_
+
+#include <CORBA/CORBA.h>
+#include "File.h"
+#include <Nirvana/static.h>
+
+NIRVANA_STATIC ((Nirvana, file_factory), Nirvana::FileFactory)
+
+#endif
