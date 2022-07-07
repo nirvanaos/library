ca0c63faa817a28361aa646f202daacbe32c4086
Igor Popov <popov.nirvana@gmail.com>
2022-07-07 20:49:39 +0300

    Add g_dec_calc core import

---
+++ CoreImport/g_dec_calc.cpp
@@ -0,0 +1,9 @@
+#include <Nirvana/Nirvana.h>
+
+namespace Nirvana {
+
+NIRVANA_OLF_SECTION
+extern const ImportInterfaceT <DecCalc> g_dec_calc = { OLF_IMPORT_INTERFACE,
+"Nirvana/g_dec_calc", CORBA::Internal::RepIdOf <DecCalc>::id };
+
+}
