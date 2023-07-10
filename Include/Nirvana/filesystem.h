4b5daee9a783008126fc4a847b52128a2bd607b1
Igor Popov <popov.nirvana@gmail.com>
2023-07-10 05:55:27 +0300

    + file system utilities

---
+++ Include/Nirvana/filesystem.h
@@ -0,0 +1,73 @@
+/// \file File system utilities.
+/*
+* Nirvana runtime library.
+*
+* This is a part of the Nirvana project.
+*
+* Author: Igor Popov
+*
+* Copyright (c) 2021 Igor Popov.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Send comments and/or bug reports to:
+*  popov.nirvana@gmail.com
+*/
+#ifndef NIRVANA_FILESYSTEM_H_
+#define NIRVANA_FILESYSTEM_H_
+#pragma once
+
+#include <CORBA/CORBA.h>
+#include <CORBA/CosNaming.h>
+
+namespace CosNaming {
+
+/// Check NameComponent structures for equality.
+inline bool operator == (const NameComponent& l, const NameComponent& r) noexcept
+{
+	return l.id () == r.id () && l.kind () == r.kind ();
+}
+
+/// Check NameComponent structures for inequality.
+inline bool operator != (const NameComponent& l, const NameComponent& r) noexcept
+{
+	return l.id () != r.id () || l.kind () != r.kind ();
+}
+
+/// Compare NameComponent structures lexicographically.
+/// Uses conversion to wide strings for the correct collation.
+bool operator < (const NameComponent& l, const NameComponent& r);
+
+}
+
+namespace Nirvana {
+
+/// Get filesystem path from compound name.
+/// 
+/// CosNaming::NamingContextEx::to_string produces incorrect result for file names
+/// because of the dot escaping. Use this function to convert name to string.
+/// 
+/// \param name Compound name.
+/// \returns Path as a string.
+IDL::String get_path_from_name (const CosNaming::Name& name);
+
+/// Checks whether the name is absolute or relative. 
+inline bool is_absolute (const CosNaming::Name& name) noexcept
+{
+	return !name.empty () && name.front ().id ().empty () && name.front ().kind ().empty ();
+}
+
+}
+
+#endif
---
+++ Source/filesystem.cpp
@@ -0,0 +1,81 @@
+/*
+* Nirvana runtime library.
+*
+* This is a part of the Nirvana project.
+*
+* Author: Igor Popov
+*
+* Copyright (c) 2021 Igor Popov.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Send comments and/or bug reports to:
+*  popov.nirvana@gmail.com
+*/
+#include <Nirvana/filesystem.h>
+#include <Nirvana/string_conv.h>
+
+namespace CosNaming {
+
+bool operator < (const NameComponent& l, const NameComponent& r)
+{
+	IDL::WString wl, wr;
+	utf8_to_wide (l.id (), wl);
+	utf8_to_wide (r.id (), wr);
+	int cmp = wl.compare (wr);
+	if (cmp < 0)
+		return true;
+	if (cmp > 0)
+		return false;
+
+	wl.clear ();
+	wr.clear ();
+	utf8_to_wide (l.id (), wl);
+	utf8_to_wide (r.id (), wr);
+	return wl < wr;
+}
+
+}
+
+namespace Nirvana {
+
+IDL::String get_path_from_name (const CosNaming::Name& name)
+{
+	IDL::String path;
+	if (!name.empty ()) {
+		size_t size = name.size () - 1;
+		for (auto& nc : name) {
+			size += nc.id ().size ();
+			if (!nc.kind ().empty ())
+				size += nc.kind ().size () + 1;
+		}
+		path.reserve (size);
+		for (auto it = name.begin ();;) {
+			path += it->id ();
+			if (!it->kind ().empty ()) {
+				path += '.';
+				path += it->kind ();
+			}
+			if (name.end () == ++it) {
+				if (path.empty ())
+					path += '/';
+				break;
+			}
+			path += '/';
+		}
+	}
+	return path;
+}
+
+}
