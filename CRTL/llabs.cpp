c55cb922efcae487e32eee1bcf439bc0c9a7c81f
Igor Popov <popov.nirvana@gmail.com>
2023-10-25 15:24:07 +0300

    Fix llabs.cpp

--- CRTL/llabs.cpp
+++ CRTL/llabs.cpp
@@ -1,3 +1,9 @@
+#include <stdlib.h>
+
+#if defined(_MSC_BUILD) && !(defined (__GNUG__) || defined (__clang__))
+#pragma function(llabs)
+#endif
+
 extern "C" long long __cdecl llabs (long long const number)
 {
 	return number >= 0 ? number : -number;
