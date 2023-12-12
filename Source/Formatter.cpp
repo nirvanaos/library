1763e53628da44ff5ea0d3c2aa0ad452417f73ae
Igor Popov <popov.nirvana@gmail.com>
2023-12-12 11:14:09 +0300

    Fix bug

--- Source/Formatter.cpp
+++ Source/Formatter.cpp
@@ -241,11 +241,11 @@ int Formatter::vformat (bool wide, CIn& fmt, va_list args, COut& out,
 							case 's': {
 								if (flags & FLAG_LONG) {
 									const wchar_t* p = va_arg (args, wchar_t*);
-									unsigned l = (unsigned)wcsnlen_s (p, precision ? precision : (size_t)-1);
+									unsigned l = (unsigned)wcsnlen_s (p, precision ? precision : std::numeric_limits <size_t>::max ());
 									count += out_string (p, l, width, precision, flags, wide, out, loc);
 								} else {
 									const char* p = va_arg (args, char*);
-									unsigned l = (unsigned)strnlen_s (p, precision ? precision : (size_t)-1);
+									unsigned l = (unsigned)strnlen_s (p, precision ? precision : std::numeric_limits <size_t>::max ());
 									count += out_string (p, l, width, precision, flags, wide, out, loc);
 								}
 							} break;
