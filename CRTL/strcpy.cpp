b3735e9593d27758343ab2197ae50fa20da0be7a
Igor Popov <popov.nirvana@gmail.com>
2023-12-17 16:13:46 +0300

    wcscpy

--- CRTL/strcpy.cpp
+++ CRTL/strcpy.cpp
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <wchar.h>
 
 #if defined(_MSC_BUILD) && !(defined (__GNUG__) || defined (__clang__))
 #pragma function(strcpy)
@@ -7,4 +8,9 @@
 extern "C" char* strcpy (char* dst, const char* src)
 {
 	return (char*)memcpy (dst, src, strlen (src) + 1);
+}
+
+extern "C" wchar_t* wcscpy (wchar_t* dst, const wchar_t* src)
+{
+	return (wchar_t*)memcpy (dst, src, (wcslen (src) + 1) * sizeof (wchar_t));
 }
