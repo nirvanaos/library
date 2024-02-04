10e090e4a485e270fe13cc8db06d78983e8e3c73
Igor Popov <popov.nirvana@gmail.com>
2024-02-04 12:58:39 +0300

    Migrate DecCalc to const interface

--- CoreImport/g_dec_calc.cpp
+++
@@ -1,7 +0,0 @@
-#include <Nirvana/Nirvana.h>
-
-namespace Nirvana {
-
-NIRVANA_IMPORT (g_dec_calc, "Nirvana/g_dec_calc", DecCalc)
-
-}
--- Mock/g_dec_calc.cpp
+++
@@ -1,8 +0,0 @@
-#include <Nirvana/Nirvana.h>
-
-namespace Nirvana {
-
-NIRVANA_SELECTANY extern
-const ImportInterfaceT <DecCalc> g_dec_calc = { OLF_IMPORT_INTERFACE };
-
-}
