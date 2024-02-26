b84ed588efc8e65ff477c3dff23c19a9313583d2
Igor Popov <popov.nirvana@gmail.com>
2024-02-26 12:31:33 +0300

    Change pseudo interface Main

--- Source/nirvana_process.cpp
+++ Source/nirvana_process.cpp
@@ -27,19 +27,52 @@
 #include <CORBA/Server.h>
 #include <Nirvana/Main_s.h>
 #include "crt_startup.h"
+#include <memory>
 
-extern int main (int argc, char* argv [], char* envp []);
+extern "C" int main (int argc, char* argv [], char* envp []);
+extern int nmain (Nirvana::Main::Strings& argv, Nirvana::Main::Strings& envp);
 
 namespace Nirvana {
+
+class ArgPtrs
+{
+public:
+	ArgPtrs (Main::Strings& strings);
+
+	~ArgPtrs ()
+	{
+		g_memory->release (pointers_, size_);
+	}
+
+	operator char** () const noexcept
+	{
+		return pointers_;
+	}
+
+private:
+	char** pointers_;
+	size_t size_;
+};
+
+ArgPtrs::ArgPtrs (Main::Strings& strings) :
+	pointers_ (nullptr),
+	size_ ((strings.size () + 1) * sizeof (char*))
+{
+	pointers_ = (char**)g_memory->allocate (nullptr, size_, 0);
+	for (size_t i = 0; i < strings.size (); ++i) {
+		pointers_ [i] = const_cast <char*> (strings [i].data ());
+	}
+	pointers_ [strings.size ()] = nullptr;
+}
 
 class ProcessMain :
 	public CORBA::servant_traits <Main>::ServantStatic <ProcessMain>
 {
 public:
-	static int32_t main (uint16_t argc, void* argv, void* envp)
+	static int32_t main (Strings& argv, Strings& envp)
 	{
 		crt_init ();
-		return ::main (argc, (char**)argv, (char**)envp);
+		return nmain (argv, envp);
 	}
 
 	static void cleanup ()
@@ -54,6 +87,11 @@ public:
 	}
 };
 
+}
+
+int nmain (Nirvana::Main::Strings& argv, Nirvana::Main::Strings& envp)
+{
+	return main ((int)argv.size (), Nirvana::ArgPtrs (argv), Nirvana::ArgPtrs (envp));
 }
 
 extern "C" NIRVANA_OLF_SECTION_OPT const Nirvana::ModuleStartup nirvana_process
