33c0d918d5558d032855f48a5caff17454a49f15
Igor Popov <popov.nirvana@gmail.com>
2025-04-05 06:14:48 +0300

    Implement pthread TLS

--- CRTL/pthread.cpp
+++ CRTL/pthread.cpp
@@ -24,32 +24,57 @@
 *  popov.nirvana@gmail.com
 */
 #include "pch/pch.h"
-
-/*
-* Nirvana C runtime library.
-*
-* This is a part of the Nirvana project.
-*
-* Author: Igor Popov
-*
-* Copyright (c) 2021 Igor Popov.
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU Lesser General Public License as published by
-* the Free Software Foundation; either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with this library.  If not, see <http://www.gnu.org/licenses/>.
-*
-* Send comments and/or bug reports to:
-*  popov.nirvana@gmail.com
-*/
-#include "pch/pch.h"
 #include <CRTL/pthread.h>
 
+extern "C" void* pthread_getspecific (pthread_key_t key)
+{
+	try {
+		return Nirvana::the_posix->CS_get (key);
+	} catch (...) {}
+	return nullptr;
+}
+
+extern "C" int pthread_key_create (pthread_key_t* key, void (*deleter)(void*))
+{
+	int ret = EINVAL;
+	try {
+		*key = Nirvana::the_posix->CS_alloc (deleter);
+		ret = 0;
+	} catch (const CORBA::SystemException& ex) {
+		int e = Nirvana::get_minor_errno (ex.minor ());
+		if (e)
+			ret = e;
+	} catch (...) {
+	}
+	return ret;
+}
+
+extern "C" int pthread_key_delete (pthread_key_t key)
+{
+	int ret = EINVAL;
+	try {
+		Nirvana::the_posix->CS_free (key);
+		ret = 0;
+	} catch (const CORBA::SystemException& ex) {
+		int e = Nirvana::get_minor_errno (ex.minor ());
+		if (e)
+			ret = e;
+	} catch (...) {
+	}
+	return ret;
+}
+
+extern "C" int pthread_setspecific (pthread_key_t key, const void* val)
+{
+	int ret = EINVAL;
+	try {
+		Nirvana::the_posix->CS_set (key, const_cast <void*> (val));
+		ret = 0;
+	} catch (const CORBA::SystemException& ex) {
+		int e = Nirvana::get_minor_errno (ex.minor ());
+		if (e)
+			ret = e;
+	} catch (...) {
+	}
+	return ret;
+}
