25f9487294a3b39b36a5a4c8d07c36db05b495e6
Igor Popov <popov.nirvana@gmail.com>
2025-05-07 09:16:31 +0300

    + ctype.cpp

--- Include/CRTL/ctype.h
+++ Include/CRTL/ctype.h
@@ -33,38 +33,40 @@ typedef struct __Locale* locale_t;
 extern "C" {
 #endif
 
-int isalnum(int);
-int isalpha(int);
+int isalnum (int);
+int isalnum_l (int, locale_t);
+int isalpha (int);
+int isalpha_l (int, locale_t);
 
 inline int isascii (int c)
 {
 	return ((unsigned)c & ~0x7F) == 0;
 }
 
-int isblank(int);
-int isblank_l(int, locale_t);
-int iscntrl(int);
-int iscntrl_l(int, locale_t);
-int isdigit(int);
-int isdigit_l(int, locale_t);
+int isblank (int);
+int isblank_l (int, locale_t);
+int iscntrl (int);
+int iscntrl_l (int, locale_t);
+int isdigit (int);
+int isdigit_l (int, locale_t);
 int isgraph (int);
 int isgraph_l (int, locale_t);
-int islower(int);
-int islower_l(int, locale_t);
-int isprint(int);
-int isprint_l(int, locale_t);
-int ispunct(int);
-int ispunct_l(int, locale_t);
+int islower (int);
+int islower_l (int, locale_t);
+int isprint (int);
+int isprint_l (int, locale_t);
+int ispunct (int);
+int ispunct_l (int, locale_t);
 int isspace (int);
 int isspace_l (int, locale_t);
-int isupper(int);
-int isupper_l(int, locale_t);
-int isxdigit(int);
-int isxdigit_l(int, locale_t);
-int tolower(int);
-int tolower_l (int c, locale_t locale);
-int toupper(int);
-int toupper_l (int c, locale_t locale);
+int isupper (int);
+int isupper_l (int, locale_t);
+int isxdigit (int);
+int isxdigit_l (int, locale_t);
+int tolower (int);
+int tolower_l (int, locale_t);
+int toupper (int);
+int toupper_l (int, locale_t);
 
 #ifdef __cplusplus
 }
