6a8ed42993decfb41978b6bb00df50baafc99f00
Igor Popov <popov.nirvana@gmail.com>
2025-08-09 23:15:31 +0300

    + __external_threading

--- CRTL/pthread.cpp
+++ CRTL/pthread.cpp
@@ -25,6 +25,7 @@
 */
 #include "pch/pch.h"
 #include <Nirvana/Module.h>
+#include <Nirvana/POSIX.h>
 #include <CRTL/pthread.h>
 
 extern "C" void* pthread_getspecific (pthread_key_t key)
@@ -78,4 +79,10 @@ extern "C" int pthread_setspecific (pthread_key_t key, const void* val)
 	} catch (...) {
 	}
 	return ret;
+}
+
+extern "C" int pthread_once (pthread_once_t *once_control, void (*init_routine)(void))
+{
+	Nirvana::the_posix->once (*once_control, init_routine);
+	return 0;
 }
