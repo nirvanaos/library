46fbe3c0f6921853777d8b89fa18f40a12cce991
Igor Popov <popov.nirvana@gmail.com>
2025-08-17 11:38:01 +0300

    stdlib.cpp

--- CRTL/stdlib.cpp
+++ CRTL/stdlib.cpp
@@ -25,3 +25,83 @@
 */
 #include "pch/pch.h"
 #include <stdlib.h>
+
+extern "C" char *getenv(const char *name)
+{
+	return nullptr; // TODO: Implement
+}
+
+extern "C" div_t div (int num, int denom)
+{
+	div_t r;
+
+	r.quot = num / denom;
+	r.rem = num % denom;
+	/*
+	 * The ANSI standard says that |r.quot| <= |n/d|, where
+	 * n/d is to be computed in infinite precision.  In other
+	 * words, we should always truncate the quotient towards
+	 * 0, never -infinity or +infinity.
+	 *
+	 * Machine division and remainer may work either way when
+	 * one or both of n or d is negative.  If only one is
+	 * negative and r.quot has been truncated towards -inf,
+	 * r.rem will have the same sign as denom and the opposite
+	 * sign of num; if both are negative and r.quot has been
+	 * truncated towards -inf, r.rem will be positive (will
+	 * have the opposite sign of num).  These are considered
+	 * `wrong'.
+	 *
+	 * If both are num and denom are positive, r will always
+	 * be positive.
+	 *
+	 * This all boils down to:
+	 *	if num >= 0, but r.rem < 0, we got the wrong answer.
+	 * In that case, to get the right answer, add 1 to r.quot and
+	 * subtract denom from r.rem.
+	 *      if num < 0, but r.rem > 0, we also have the wrong answer.
+	 * In this case, to get the right answer, subtract 1 from r.quot and
+	 * add denom to r.rem.
+	 */
+	if (num >= 0 && r.rem < 0) {
+		++r.quot;
+		r.rem -= denom;
+	}
+	else if (num < 0 && r.rem > 0) {
+		--r.quot;
+		r.rem += denom;
+	}
+	return (r);
+}
+
+extern "C" ldiv_t ldiv (long num, long denom)
+{
+	ldiv_t r;
+
+	/* see div.c for comments */
+
+	r.quot = num / denom;
+	r.rem = num % denom;
+	if (num >= 0 && r.rem < 0) {
+		++r.quot;
+		r.rem -= denom;
+	}
+	else if (num < 0 && r.rem > 0) {
+		--r.quot;
+		r.rem += denom;
+	}
+	return (r);
+}
+
+extern "C" lldiv_t lldiv (long long numer, long long denom)
+{
+	lldiv_t retval;
+
+	retval.quot = numer / denom;
+	retval.rem = numer % denom;
+	if (numer >= 0 && retval.rem < 0) {
+		retval.quot++;
+		retval.rem -= denom;
+	}
+	return (retval);
+}
