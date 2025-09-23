49a70df866608066b99f8752ed9f27d8b14d53a4
Igor Popov <popov.nirvana@gmail.com>
2025-09-23 16:16:24 +0300

    external_threading dummy condvar

--- CRTL/Source/impl/external_threading.cpp
+++ CRTL/Source/impl/external_threading.cpp
@@ -7,25 +7,21 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 
 int __libcpp_condvar_signal (__libcpp_condvar_t*)
 {
-	NIRVANA_UNREACHABLE_CODE ();
 	return 0;
 }
 
 int __libcpp_condvar_wait (__libcpp_condvar_t*, __libcpp_mutex_t*)
 {
-	NIRVANA_UNREACHABLE_CODE ();
 	return 0;
 }
 
 int __libcpp_condvar_timedwait (__libcpp_condvar_t*, __libcpp_mutex_t*, __libcpp_timespec_t*)
 {
-	NIRVANA_UNREACHABLE_CODE ();
 	return 0;
 }
 
 int __libcpp_condvar_destroy (__libcpp_condvar_t*)
 {
-	NIRVANA_UNREACHABLE_CODE ();
 	return 0;
 }
 
