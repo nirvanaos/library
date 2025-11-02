9ef317650bbfc5b915b280dda008a7889f97dd06
Igor Popov <popov.nirvana@gmail.com>
2025-11-02 14:45:49 +0300

    Fix abort()

--- CRTL/Source/abort.cpp
+++ CRTL/Source/abort.cpp
@@ -34,8 +34,6 @@ extern "C"
 NIRVANA_NORETURN
 void abort (void)
 {
-	Bridge <Nirvana::POSIX>* br = static_cast <Bridge <Nirvana::POSIX>*> (
-		&static_cast <I_ptr <Nirvana::POSIX> > (Nirvana::the_posix));
-	br->_epv ().epv.raise (br, SIGABRT, nullptr);
+	Nirvana::the_posix->raise (SIGABRT);
 	NIRVANA_UNREACHABLE_CODE ();
 }
